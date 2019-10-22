385cf23a6e1ae7a6c768e35432e386951796d1e0
Brian Duff <brian21495@gmail.com>
2019-10-21 21:43:24 -0500

    push

---
+++ RaceTrack/Cars/Tesla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Tesla : RaceCar
+    {
+        public Tesla()
+        {
+            Name = "Tesla Model S";
+            TopSpeed = 110;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} is electrified and fully charged!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} refuses to drop Tesla stock prices by slowing!");
+            base.Brake();
+        }
+
+
+    }
+}
---
+++ RaceTrack/Drivers/Brian.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Brian : Driver
+    {
+        public Brian(RaceCar car) : base(car)
+        {
+            Name = "Brian";
+            SkillLevel = 10;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+
+    }
+}
